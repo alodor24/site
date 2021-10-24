1727258a6e90f9ebbcf8c966f60f2960cd6923ad
Jose Alejandro <jose-a-mendez@hotmail.com>
2021-10-24 17:37:30 -0300

    Integrated fontawesome on project

--- components/Head.jsx
+++ components/Head.jsx
@@ -7,6 +7,7 @@ const HeadApp = () => {
             <meta name="description" content="Diseño y desarrollo de aplicaciones web con estandares actuales de UI/UX, Seguridad y Compatibilidad con distintos navegadores y dispositivos." />
             <meta name="keywords" content="diseño web, desarrollo de páginas web, elaboración de sitios web, diseño web, alodor, desarrollo de app, comercio electrónico, tienda virtual, crear carrito de compra, diseño responsive, blog, aplicaciones web" />
             <link rel="icon" href="/assets/images/favicon.ico" />
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous" />
             <title>Alodor | Portafolio Profesional</title>
         </Head>
     )
